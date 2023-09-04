20a5a4c2f9e44d1e2b7aea068dc73ef472b5767c
Samantha Dangu <dangu.samantha@gmail.com>
2023-09-04 18:54:34 +0200

    gestion d'erreur sur les appels api

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -1,21 +1,24 @@
 import Pokemon from "../models/pokemon";
- 
-export default class PokemonService {
- 
-  static getPokemons(): Promise<Pokemon[]> {
-    return fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon')
-      .then(response => response.json());
-  }
- 
-  static getPokemon(id: number): Promise<Pokemon|null> {
-    return fetch(`https://api-pokemon-fr.vercel.app/api/v1/pokemon/${id}`)
 
-      .then(response => response.json())
-      .then(data => this.isEmpty(data) ? null : data);
-      
+export default class PokemonService {
+  static getPokemons(): Promise<Pokemon[]> {
+    return fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
+      .then((response) => response.json())
+      .catch((error) => this.handleError(error));
   }
- 
+
+  static getPokemon(id: number): Promise<Pokemon | null> {
+    return fetch(`https://api-pokemon-fr.vercel.app/api/v1/pokemon/${id}`)
+      .then((response) => response.json())
+      .then((data) => (this.isEmpty(data) ? null : data))
+      .catch((error) => this.handleError(error));
+  }
+
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
+  }
+
+  static handleError(error: Error): void {
+    console.error(error);
   }
 }
