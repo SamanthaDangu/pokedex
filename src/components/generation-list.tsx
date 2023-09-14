bbe85c4b821d851cd25a37f5d6b80f9f17a6520f
Samantha Dangu <dangu.samantha@gmail.com>
2023-09-14 19:50:32 +0200

    responsive la liste

--- src/components/generation-list.tsx
+++ src/components/generation-list.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./generation-list.css"
+import "./generation-list.css";
 import formatGeneration from "../utils/format-generation";
 import Pokemon from "../models/pokemon";
 
@@ -10,11 +10,15 @@ interface GenerationListProps {
 const GenerationList: React.FC<GenerationListProps> = ({ generations }) => {
   return (
     <div className="generations-links">
-      {Object.keys(generations).map((generation) => (
-        <a key={generation} href={`#generation-${generation}`} id="generation-link">
-          {formatGeneration(Number(generation))}
-        </a>
-      ))}
+      <div className="row">
+        {Object.keys(generations).map((generation) => (
+          <div key={generation} className="col s12 m6 l2">
+            <a href={`#generation-${generation}`} id="generation-link">
+              {formatGeneration(Number(generation))}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
