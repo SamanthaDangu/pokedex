d4dcaf59598edb848cfde4fd3d43df5ee4f1008a
Samantha Dangu <dangu.samantha@gmail.com>
2023-09-14 19:51:47 +0200

    Ajout du logo et du style

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,21 @@ import "./App.css";
 const App: FunctionComponent = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <div className="nav-wrapper teal">
-            <Link to="/" className="brand-logo center">
-              Pokédex
-            </Link>
-          </div>
-        </nav>
+      <div className="container center" style={{ boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)", padding: "32px", backgroundColor: "#fffde7"}}>
+        <header
+          className="center"
+          style={{
+            height: "auto",
+          }}
+        >
+          <Link to="/" className="brand-logo center">
+            <img
+              className="responsive-img image-pokemon"
+              src="./assets/International_Pokémon_logo.svg.png"
+              alt=""
+            />
+          </Link>
+        </header>
 
         <Switch>
           <Route exact path="/" component={PokemonList} />
@@ -23,6 +30,14 @@ const App: FunctionComponent = () => {
           <Route path="/pokemons/:id" component={PokemonsDetail} />
           <Route component={PageNotFound} />
         </Switch>
+
+        <footer>
+          <p>
+            © Pokémon est une marque déposée de The Pokémon Company
+            International, Game Freak et Nintendo. Les images appartiennent à
+            leur auteur respectif. Site fait par un fan.
+          </p>
+        </footer>
       </div>
     </Router>
   );
